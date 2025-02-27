123504d5c4e19462ed3bf4cf5aebc7624c1ea94e
Ray <Ray.yoo@beckhoff.com>
2025-02-27 17:33:54 +0900

    sample

---
+++ CodeBehindJs1.js
@@ -0,0 +1,34 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../Packages/Beckhoff.TwinCAT.HMI.Framework.14.2.110/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    // If you want to unregister an event outside the event code you need to use the return value of the method register()
+    let destroyOnInitialized = TcHmi.EventProvider.register('onInitialized', (e, data) => {
+        // This event will be raised only once, so we can free resources. 
+        // It's best practice to use destroy function of the event object within the callback function to avoid conflicts.
+        e.destroy();
+        // ----------------------
+        // Place your code here!
+        // ----------------------
+        let variableSomething = false
+
+        // TwinCAT HMI textbox element의 구조 모양
+        const textBoxHMI = document.getElementById('TcHmiTextbox_1').childNodes[0].childNodes[1].childNodes[1].childNodes[0]
+        const textHMI = document.getElementById('TcHmiTextblock_1')
+        const buttonHMI = document.getElementById('TcHmiButton_1')
+
+        buttonHMI.addEventListener('click', () => {
+            variableSomething = !variableSomething
+            textHMI.innerText = String(variableSomething)
+            if (variableSomething)
+                textBoxHMI.focus()
+            else
+                textBoxHMI.blur()
+        })
+
+        textBoxHMI.addEventListener('blur', () => {
+            if (variableSomething)
+                textBoxHMI.focus()
+        })
+    });
+})(TcHmi);
